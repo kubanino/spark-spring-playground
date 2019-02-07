daa460db05a2836712e1b1abf35031aefb08c966
Jakub Dubański <jakub.dubanski@gmail.com>
2019-02-08 00:16:28 +0100

    [SparkSP] Stupid simple spark app to submit and test newbie things.

--- src/main/java/com/public_class/spark_spring/Main.java
+++ src/main/java/com/public_class/spark_spring/Main.java
@@ -1,9 +1,37 @@
 package com.public_class.spark_spring;
 
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.SparkSession;
+
+import java.io.IOException;
+
 public class Main
 {
     public static void main(String[] args)
     {
+        final String logFile = "C:\\spark-2.2.1-bin-hadoop2.7\\README.md";
+        final SparkSession spark = SparkSession.builder().appName("Simple Application").getOrCreate();
+        final Dataset<String> logData = spark.read().textFile(logFile).cache();
 
+        long numAs = logData.filter(s -> s.contains("a")).count();
+        long numBs = logData.filter(s -> s.contains("b")).count();
+
+        System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
+
+        stopMeUntilGodPressedEnter();
+
+        spark.stop();
+    }
+
+    private static void stopMeUntilGodPressedEnter()
+    {
+        try
+        {
+            System.in.read();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
